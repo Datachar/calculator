f651dd82d7e3482797913d0f8e0184d9eba3a73b
Mykhailo Huzyuk <g.and.m@ukr.net>
2016-02-06 14:53:11 +0200

    New

--- log.py
+++ log.py
@@ -1,18 +1,5 @@
 def ln(x):
-    if x == 0:
-        return float('inf')
-    if x > 1:
-        size_ = len(str(int(x)))
-        x /= x - 1
-        if size_ > 6:
-            size_ = 6
-        sum = 0
-        for i in range(1,20*10**size_):
-            sum += 1/(i*x**i)
-        return sum
-    else:
-        x = x-1
-        sum = 0
-        for i in range(1,10000):
-            sum += ((-1)**(i+1)*x**i)/i
-        return sum
+    sum_ = 0
+    for i in range(0, 100000):
+        sum_ += (1 / (2 * i + 1)) * ((x - 1) / (x + 1)) ** (2 * i + 1)
+    return sum_ * 2
--- test_log.py
+++ test_log.py
@@ -10,9 +10,9 @@ class TestStrToList(TestCase):
             (0.012358, math.log(0.012358)),
             (1, math.log(1)),
             (12345, math.log(12345)),
-            (9.999999999999, math.log(9.999999999999))
-            #(704645, math.log(704645)),
-            #(0.0000000001, math.log(0.0000000001))
+            (9.999999999999, math.log(9.999999999999)),
+            (18012.3254, math.log(18012.3254)),
+            (0.00008, math.log(0.00008))
         ]
 
         for equation, expected_result in equations:
