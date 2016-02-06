c4d270986cea4a394ee82de992fa1ae93a84c006
Mykhailo Huzyuk <g.and.m@ukr.net>
2016-02-06 13:40:24 +0200

    Fix some bug

--- ln_algorithm.py
+++ ln_algorithm.py
@@ -1,7 +1,18 @@
 def ln(x):
-    size_ = len(str(int(x)))
-    x /= x - 1
-    sum = 0
-    for i in range(1,100*10**size_):
-        sum += 1/(i*x**i)
-    return sum
+    if x == 0:
+        return float('inf')
+    if x > 1:
+        size_ = len(str(int(x)))
+        x /= x - 1
+        if size_ > 6:
+            size_ = 6
+        sum = 0
+        for i in range(1,20*10**size_):
+            sum += 1/(i*x**i)
+        return sum
+    else:
+        x = x-1
+        sum = 0
+        for i in range(1,10000):
+            sum += ((-1)**(i+1)*x**i)/i
+        return sum
